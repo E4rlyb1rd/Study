474d39f77e1e3013ba614d588d824ce618a66f2c
E4rlyB1rd <sangkwon00320@gmail.com>
2018-10-09 14:55:32 +0900

    sum
    sum

---
+++ day3/sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+
+	int i=0;
+	int j=0;
+	int sum=0;
+
+	scanf("%d",&i);
+
+	if(i<=100){
+		while(j<=i){
+			sum=sum+j;
+			j++;
+		}
+		printf("%d",t);
+	}
+}
